dfb98fa1002522406846f6a505ee85170d11bb86
Adam Jensen <acjensen@gmail.com>
2019-07-21 18:01:17 -0400

    Update to Elixir 1.8

--- mix.exs
+++ mix.exs
@@ -4,7 +4,7 @@ defmodule PagerdutyEx.Mixfile do
   def project do
     [app: :pagerduty_ex,
      version: "0.1.0",
-     elixir: "~> 1.4",
+     elixir: "~> 1.8",
      elixirc_paths: elixirc_paths(Mix.env),
      build_embedded: Mix.env == :prod,
      start_permanent: Mix.env == :prod,
